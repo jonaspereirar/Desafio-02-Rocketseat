2b52dcf869578c9e40532a7d2c013ce808ea78de
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-07-26 17:52:21 +0100

    feat: update  aap.js and routes

--- src/app.js
+++ src/app.js
@@ -1,59 +1,19 @@
 import express from 'express';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import routes from './routes';
 
 this.middewares();
 this.routes();
 
-class App {
-  constructor() {
-    this.server = express();
   }
 
   middewares() {
-    this.server.use(express.json);
-=======
-import routes from './routes';
-
-class App {
-  constructor() {
-    this.server = express();
-
-    this.middeware();
-    this.routes();
-  }
-
-  middeware() {
     this.server.use(express.json());
 >>>>>>> server
   }
 
   routes() {
     this.server.use(routes);
-  }
+  };
 }
-=======
-
-import routes from './routes';
-
-class App {
-    constructor() {
-        this.server = express();
-
-        this.middewares();
-        this.routes();
-    }
-
-    middewares() {
-        this.server.use(express.json());
-    }
-
-    routes() {
-        this.server.use(routes);
-    }
-}
-
->>>>>>> featrc
 export default new App().server;
