0d46beb9ff9dbad30cfed5308dba69e4449ed626
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-07-26 17:52:52 +0100

    feat: updates app.js and routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,18 @@ import express from 'express';
 
 import routes from './routes';
 
-this.middewares();
-this.routes();
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middewares();
+    this.routes();
 
   }
 
   middewares() {
     this.server.use(express.json());
->>>>>>> server
-  }
-
+  };
   routes() {
     this.server.use(routes);
   };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ Menssagem: 'Olá galera' });
+    return res.json({ Menssagem: 'Olá Pessoal' });
 });
 
 export default routes;
