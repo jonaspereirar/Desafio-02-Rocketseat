93c08c46c6dc3c14bf3c2e7ffa45c3f65a7d4834
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-07-29 23:45:21 +0100

    feat: add sequelize and creating user successful

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
+// Rota teste
 routes.get('/', (req, res) => {
-    return res.json({ Menssagem: 'Olá Pessoal' });
+    return res.json({ mensagem: 'Olá adorei isso aqui' });
 });
+
+// Rota de criação de usuario
+routes.post('/users', UserController.store);
 
 export default routes;
