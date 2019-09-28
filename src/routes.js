30dcbc7a81591866d69f393f04943adc0e23ee13
jprodrigues1981 <jonasprodrigues@gmail.com>
2019-09-28 22:25:21 +0100

    feature add: add multer and function upload file

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
+
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 // Rota de criação de usuario
 routes.post('/users', UserController.store);
@@ -15,5 +21,8 @@ routes.use(authMiddleware); // User Authentication Middleware Logged In.
 
 // after logged in user can update
 routes.put('/users', UserController.update);
+
+// update file
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
